131de0c29f1b33ed4a0b57c4174bfa16245745ff
Adam Ormsby <ormsbyadam@gmail.com>
2025-11-29 14:54:35 -0600

    Update template with part util and time tracking

--- src/Utils.kt
+++ src/Utils.kt
@@ -2,6 +2,19 @@ import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readText
+import kotlin.time.measureTimedValue
+
+fun day(i: Int, title: String) {
+    println("Day $i: $title")
+}
+
+fun <T> part(i: Int, input: List<String>, block: (List<String>) -> T): T {
+    val (output, duration) = measureTimedValue {
+        block(input)
+    }
+    println("Part $i --> ${duration.inWholeMilliseconds}ms")
+    return output
+}
 
 /**
  * Reads lines from the given input txt file.
