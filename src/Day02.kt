5345ad2021b59b42f6f60af439d37a92187c0b39
Adam Ormsby <ormsbyadam@gmail.com>
2025-12-01 03:23:27 -0600

    add copies

---
+++ src/Day02.kt
@@ -0,0 +1,14 @@
+fun main() {
+    day(1, "TBD")
+
+    val input = readInput("Day02")
+//    val input = readInput("Day02_test")
+
+    part(1, input) {
+        null
+    }.also { println("TBD, $it") }
+
+    part(2, input) {
+        null
+    }.also { println("TBD, $it") }
+}
---
+++ src/Day03.kt
@@ -0,0 +1,14 @@
+fun main() {
+    day(1, "TBD")
+
+    val input = readInput("Day03")
+//    val input = readInput("Day03_test")
+
+    part(1, input) {
+        null
+    }.also { println("TBD, $it") }
+
+    part(2, input) {
+        null
+    }.also { println("TBD, $it") }
+}
