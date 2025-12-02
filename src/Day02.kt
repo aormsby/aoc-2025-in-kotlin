7a781e6c148ddd557ef2b11997e6d4b4804b30fe
Adam Ormsby <ormsbyadam@gmail.com>
2025-12-02 00:20:48 -0600

    add d2p2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,3 +1,5 @@
+import kotlin.math.sqrt
+
 fun main() {
     day(2, "TBD")
 
@@ -24,6 +26,31 @@ fun main() {
     }.also { println("Mirrored IDs, $it") }
 
     part(2, input) {
-        2
+        input.fold(0L) { acc, strRange ->
+            val (n, m) = strRange.split('-')
+            val r = n.toLong()..m.toLong()
+            val iter = r.iterator()
+            var next = acc
+
+            while (iter.hasNext()) {
+                val cur = iter.next()
+                with(cur.toString()) {
+                    if (length != 1) {
+                        for (i in 1..sqrt(length.toFloat()).toInt()) {
+                            if (length % i == 0) {
+                                val other = length / i
+                                if (chunked(i).distinct().size == 1
+                                    || (other != length && chunked(other).distinct().size == 1)
+                                ) {
+                                    next += cur
+                                    break
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            next
+        }
     }.also { println("Repeating IDs, $it") }
 }
