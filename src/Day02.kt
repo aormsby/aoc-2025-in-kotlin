78f93d6903cf8e382e101ea247dcf104d50e0793
Adam Ormsby <ormsbyadam@gmail.com>
2025-12-01 23:50:16 -0600

    add d2p1

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,14 +1,29 @@
 fun main() {
-    day(1, "TBD")
+    day(2, "TBD")
 
-    val input = readInput("Day02")
-//    val input = readInput("Day02_test")
+    val input = splitInput("Day02", ",")
+//    val input = splitInput("Day02_test", ",")
 
     part(1, input) {
-        null
-    }.also { println("TBD, $it") }
+        input.fold(0L) { acc, strRange ->
+            val (n, m) = strRange.split('-')
+            val r = n.toLong()..m.toLong()
+            val iter = r.iterator()
+            var next = acc
+
+            while (iter.hasNext()) {
+                val cur = iter.next()
+                with(cur.toString()) {
+                    if (length % 2 == 0 && substring(0, length / 2) == substring(length / 2, length)) {
+                        next += cur
+                    }
+                }
+            }
+            next
+        }
+    }.also { println("Mirrored IDs, $it") }
 
     part(2, input) {
-        null
-    }.also { println("TBD, $it") }
+        2
+    }.also { println("Repeating IDs, $it") }
 }
