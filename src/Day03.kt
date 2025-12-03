5d8a90882daf717f888b13c64f8d295b163e8f75
Adam Ormsby <ormsbyadam@gmail.com>
2025-12-03 01:16:16 -0600

    cleanup

--- src/Day03.kt
+++ src/Day03.kt
@@ -5,60 +5,64 @@ fun main() {
 //    val input = readInput("Day03_test")
 
     part(1, input) {
-        input.sumOf { strBank ->
-            val intBank = strBank.map { it.digitToInt() }
-            val size = intBank.size
+        input.sumOf { inp ->
+            val bank = inp.map { it.digitToInt() }
+            val bSize = bank.size
             val pack = MutableList(2) { 0 }
-            var mark = 0
+            var pMark = 0
 
-            for (i in 0 until size) {
-                val cur = intBank[i]
-                val remaining = size - 1 - i
+            for (i in 0 until bSize) {
+                val cur = bank[i]
+                val bankRemaining = bSize - 1 - i
 
                 // check for lower value
-                val lowerNumI = pack.indexOfFirst { it != 0 && it < cur }
-                if (lowerNumI != -1) {
-                    val remainingFromLowerI = 1 - lowerNumI
-                    mark = 1 - minOf(remainingFromLowerI, remaining)
-                    for (j in mark..1) {
+                val iLowerNum = pack.indexOfFirst { it != 0 && it < cur }
+                if (iLowerNum != -1) {
+                    val remainingFromLower = 1 - iLowerNum
+                    pMark = 1 - minOf(remainingFromLower, bankRemaining)
+                    for (j in pMark..1) {
                         pack[j] = 0
                     }
                 }
-                if (mark <= 1) pack[mark] = cur
-                mark = (mark + 1)
+
+                // add value to pack
+                if (pMark <= 1) pack[pMark] = cur
+                pMark = (pMark + 1)
             }
 
-//            println(pack.joinToString("").toLong())
-            pack.joinToString("").toLong()
+            pack.joinToString("").toInt()
+//                .also { println(it) }
         }
     }.also { println("Sum Max Joltage 2, $it") }
 
     part(2, input) {
-        input.sumOf { strBank ->
-            val intBank = strBank.map { it.digitToInt() }
-            val size = intBank.size
+        input.sumOf { inp ->
+            val bank = inp.map { it.digitToInt() }
+            val bSize = bank.size
             val pack = MutableList(12) { 0 }
-            var mark = 0
+            var pMark = 0
 
-            for (i in 0 until size) {
-                val cur = intBank[i]
-                val remaining = size - 1 - i
+            for (i in 0 until bSize) {
+                val cur = bank[i]
+                val bankRemaining = bSize - 1 - i
 
                 // check for lower value
-                val lowerNumI = pack.indexOfFirst { it != 0 && it < cur }
-                if (lowerNumI != -1) {
-                    val remainingFromLowerI = 11 - lowerNumI
-                    mark = 11 - minOf(remainingFromLowerI, remaining)
-                    for (j in mark..11) {
+                val iLowerNum = pack.indexOfFirst { it != 0 && it < cur }
+                if (iLowerNum != -1) {
+                    val remainingFromLower = 11 - iLowerNum
+                    pMark = 11 - minOf(remainingFromLower, bankRemaining)
+                    for (j in pMark..11) {
                         pack[j] = 0
                     }
                 }
-                if (mark <= 11) pack[mark] = cur
-                mark = (mark + 1)
+
+                // add value to pack
+                if (pMark <= 11) pack[pMark] = cur
+                pMark = (pMark + 1)
             }
 
-//            println(pack.joinToString("").toLong())
             pack.joinToString("").toLong()
+//                .also { println(it) }
         }
     }.also { println("Sum Max Joltage 12, $it") }
 }
