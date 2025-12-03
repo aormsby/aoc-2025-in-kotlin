ac5e78db52cf09316e6537e6cd0652326fcd5627
Adam Ormsby <ormsbyadam@gmail.com>
2025-12-03 00:47:53 -0600

    d2p1

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,12 +1,18 @@
 fun main() {
-    day(3, "TBD")
+    day(3, "Lobby")
 
     val input = readInput("Day03")
 //    val input = readInput("Day03_test")
 
     part(1, input) {
-        null
-    }.also { println("TBD, $it") }
+        input.sumOf { strBank ->
+            val intBank = strBank.map { it.digitToInt() }
+            val first = intBank.dropLast(1).max()
+            val second = intBank.drop(intBank.indexOf(first) + 1).max()
+//            println("$first$second")
+            ("$first$second").toInt()
+        }
+    }.also { println("Sum Max Joltage 2, $it") }
 
     part(2, input) {
         null
