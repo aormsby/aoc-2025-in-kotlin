70dff8b0a180f59d4c507545d98a15e90ac1e240
Adam Ormsby <ormsbyadam@gmail.com>
2025-12-03 23:58:28 -0600

    pull out neighbor utils

--- src/Utils.kt
+++ src/Utils.kt
@@ -8,7 +8,7 @@ fun day(i: Int, title: String) {
     println("Day $i: $title")
 }
 
-fun <T> part(i: Int, input: List<String>, block: (List<String>) -> T): T {
+fun <TInput, TResult> part(i: Int, input: TInput, block: (TInput) -> TResult): TResult {
     val (output, duration) = measureTimedValue {
         block(input)
     }
@@ -27,8 +27,59 @@ fun readInput(name: String) = Path("src/$name.txt").readText().trim().lines()
 fun splitInput(name: String, separator: String) = Path("src/$name.txt").readText().trim().split(separator)
 
 /**
+ * Reads lines into 2D array of chars from the given text file.
+ */
+fun array2DInput(name: String) = readInput(name).map { line -> line.toMutableList() }
+
+/**
  * Converts string to md5 hash.
  */
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
+
+/**
+ * Quick pairs to add to current array coords to get vertical and horizontal neighbors.
+ */
+val crossNeighbors = listOf(
+    -1 to 0, 1 to 0, 0 to -1, 0 to 1,
+)
+
+/**
+ * Quick pairs to add to current array coords to get diagonal neighbors.
+ */
+val diagNeighbors = listOf(
+    -1 to 1, 1 to 1, 1 to -1, -1 to -1,
+)
+
+/**
+ * Quick pairs to add to current array coords to get all neighbors.
+ */
+val allNeighbors = crossNeighbors + diagNeighbors
+
+/**
+ * Helper to filter out invalid coordinates for neighbors that would be out of bounds.
+ */
+fun validNeighbors(
+    numRows: Int,
+    rowLength: Int,
+    rowI: Int? = null,
+    colI: Int? = null,
+    startNeighbors: List<Pair<Int, Int>>? = allNeighbors,
+): List<Pair<Int, Int>> {
+    var n = startNeighbors ?: allNeighbors
+
+    n = when (rowI) {
+        0 -> n.filterNot { it.first == -1 }
+        numRows - 1 -> n.filterNot { it.first == 1 }
+        else -> n
+    }
+
+    n = when (colI) {
+        0 -> n.filterNot { it.second == -1 }
+        rowLength - 1 -> n.filterNot { it.second == 1 }
+        else -> n
+    }
+
+    return n
+}
