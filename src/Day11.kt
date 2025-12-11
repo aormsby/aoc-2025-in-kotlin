e2bd35ad51583b3df6aca5766bc15ae446255a41
Adam Ormsby <ormsbyadam@gmail.com>
2025-12-11 00:33:01 -0600

    d11p2

--- src/Day11.kt
+++ src/Day11.kt
@@ -27,7 +27,44 @@ fun main() {
         dfs("you")
     }.also { println("Paths OUT, $it") }
 
-    part(2, input) { input ->
-        null
-    }.also { println("asdf, $it") }
+    part(2, input) { inp ->
+        val outputMap = mutableMapOf<String, List<String>>()
+        inp.forEach { line ->
+            outputMap[line.substringBefore(':')] =
+                line.substringAfter(": ").split(' ')
+        }
+        val memoPaths = mutableMapOf<Triple<String, Boolean, Boolean>, Long>()
+
+        fun dfs(curKey: String, curPath: ArrayDeque<String>, hasFFT: Boolean, hasDAC: Boolean): Long {
+            if (curKey in curPath)
+                return 0L
+
+            val fft = hasFFT || curKey == "fft"
+            val dac = hasDAC || curKey == "dac"
+
+            //
+            val memoKey = Triple(curKey, fft, dac)
+            memoPaths[memoKey]?.let { numPaths ->
+                return numPaths
+            }
+
+            curPath.addLast(curKey)
+            var pathsOut = 0L
+            val outs = outputMap[curKey] ?: emptyList()
+
+            for (next in outs) {
+                if (next == "out" && fft && dac) {
+                    pathsOut += 1
+                } else {
+                    pathsOut += dfs(next, curPath, fft, dac)
+                }
+            }
+
+            curPath.removeLast()
+            memoPaths[memoKey] = pathsOut
+            return pathsOut
+        }
+
+        dfs("svr", ArrayDeque(), hasFFT = false, hasDAC = false)
+    }.also { println("FFT/DAC Paths OUT, $it") }
 }
